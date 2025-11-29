23b8ac1b98b61bf0afe8dca9ff0910ecf19a8391
tribagus6 <tribagus325@gmail.com>
2025-11-29 11:43:32 +0000

    adjust the script

--- scripts/unseal-follower.sh
+++ scripts/unseal-follower.sh
@@ -1,6 +1,9 @@
+echo "[+] Unsealing Vault-1..."
+UNSEAL_KEY=$(grep 'Unseal Key 1:' scripts/unseal_keys.txt | awk '{print $4}' | sed 's/\x1b\[[0-9;]*m//g' | tr -d '\r' | tr -d '\n' | tr -d '[:space:]')
+docker exec -it vault-1 vault operator unseal "$UNSEAL_KEY"
+
 echo "[+] Unsealing Vault-2..."
-UNSEAL_KEY=$(grep 'Unseal Key 1:' scripts/unseal_keys.txt | awk '{print $4}')
-docker exec -it vault-2 vault operator unseal $UNSEAL_KEY
+docker exec -it vault-2 vault operator unseal "$UNSEAL_KEY"
 
 echo "[+] Unsealing Vault-3..."
-docker exec -it vault-3 vault operator unseal $UNSEAL_KEY
+docker exec -it vault-3 vault operator unseal "$UNSEAL_KEY"
